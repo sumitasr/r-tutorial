add016128cc2b5ea956039a8a8d3e1c3be16b174
Sumit Bansal <sumit.asr@gmail.com>
2016-10-07 00:39:50 +0530

    some more functions used

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # r-tutorial
+
+Requirements:
+1. Install R
+2. Install package caret with command install.packages("caret")
--- app.R
+++ app.R
@@ -3,6 +3,21 @@ filename <- "/Users/sumit/GitHub/r-tutorial/iris.csv"
 dataset <- read.csv(filename, header=FALSE)
 colnames(dataset) <- c("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width","Species")
 validation_index <- createDataPartition(dataset$Species, p=0.80, list=FALSE)
+
+# select 20% of the data for validation
 validation <- dataset[-validation_index,]
+
+# use the remaining 80% of data to training and testing the models
 dataset <- dataset[validation_index,]
+
+# dimensions of dataset
 dim(dataset)
+
+# list types for each attribute
+sapply(dataset, class)
+
+# take a peek at the first 5 rows of the data
+head(dataset)
+
+# list the levels for the class
+levels(dataset$Species)

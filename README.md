907fbece70e858956df78439f0bb7c7f9d8f082a
Sumit Bansal <sumit.asr@gmail.com>
2016-10-07 17:15:09 +0530

    5 Models build

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 Requirements:
 1. Install R
-2. Install package caret with command install.packages("caret")
+2. install.packages("caret")
+3. install.packages("ellipse")
+4. install.packages('e1071', dependencies=TRUE)
+5. install.packages("kernlab")
+6. Fix a issue while installing kernlab package with :
+   curl -O http://r.research.att.com/libs/gfortran-4.8.2-darwin13.tar.bz2
+   sudo tar fvxz gfortran-4.8.2-darwin13.tar.bz2 -C /
--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 library(caret)
+library(e1071)
 filename <- "/Users/sumit/GitHub/r-tutorial/iris.csv"
 dataset <- read.csv(filename, header=FALSE)
 colnames(dataset) <- c("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width","Species")
@@ -21,3 +22,59 @@ head(dataset)
 
 # list the levels for the class
 levels(dataset$Species)
+
+# summarize the class distribution
+percentage <- prop.table(table(dataset$Species)) * 100
+cbind(freq=table(dataset$Species), percentage=percentage)
+
+# summarize attribute distributions
+summary(dataset)
+
+# split input and output
+x <- dataset[,1:4]
+y <- dataset[,5]
+
+# boxplot for each attribute on one image
+par(mfrow=c(1,4))
+for(i in 1:4) {
+  boxplot(x[,i], main=names(iris)[i])
+}
+
+# barplot for class breakdown
+plot(y)
+
+# scatterplot matrix
+featurePlot(x=x, y=y, plot="ellipse")
+
+# box and whisker plots for each attribute
+featurePlot(x=x, y=y, plot="box")
+
+# density plots for each attribute by class value
+scales <- list(x=list(relation="free"), y=list(relation="free"))
+featurePlot(x=x, y=y, plot="density", scales=scales)
+
+# Run algorithms using 10-fold cross validation
+control <- trainControl(method="cv", number=10)
+metric <- "Accuracy"
+
+# a) linear algorithms
+set.seed(7)
+fit.lda <- train(Species~., data=dataset, method="lda", metric=metric, trControl=control)
+# b) nonlinear algorithms
+# CART
+set.seed(7)
+fit.cart <- train(Species~., data=dataset, method="rpart", metric=metric, trControl=control)
+# kNN
+set.seed(7)
+fit.knn <- train(Species~., data=dataset, method="knn", metric=metric, trControl=control)
+# c) advanced algorithms
+# SVM
+set.seed(7)
+fit.svm <- train(Species~., data=dataset, method="svmRadial", metric=metric, trControl=control)
+# Random Forest
+set.seed(7)
+fit.rf <- train(Species~., data=dataset, method="rf", metric=metric, trControl=control)
+
+#summarize accuracy of models
+results <- resamples(list(lda=fit.lda, cart=fit.cart, knn=fit.knn, svm=fit.svm, rf=fit.rf))
+summary(results)
